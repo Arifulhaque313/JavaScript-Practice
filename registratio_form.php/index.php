e0fb088fdc12ea3434b453633469c7cf54e9bd71
Arifulhaque313 <asajib7654@gmail.com>
2022-02-09 11:11:52 +0600

    add event handler

---
+++ registratio_form.php/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    if(isset($_POST['register'])){
+
+        $name = $_POST['username'];
+        $select = $_POST['select'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $password_confirm = $_POST['password_confirm'];
+       
+
+        echo $name ;
+
+
+    }
+
+
+?>
