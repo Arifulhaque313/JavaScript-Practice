316d1044f7eefa8a70883cc21615fdfeaed8f088
Arifulhaque313 <asajib7654@gmail.com>
2022-02-21 10:58:47 +0600

    new commit

--- Simple_bank_using_JavaScript/js/banking.js
+++ Simple_bank_using_JavaScript/js/banking.js
@@ -1,39 +1,93 @@
+// get input from the input field 
+function getInput(input) {
+    const inputId = document.getElementById(input);
+    const inputValue = parseFloat(inputId.value);
+    inputId.value = ' ';
+    return inputValue;
+}
 
-// add deposite value
 
-document.getElementById('add-deposite').addEventListener('click', function () {  //deposite button id = add-deposite
+// update the value thats we get from the inputfield
+function updateField(depositeId, newDepositeValue) {
 
-    const totalDeposite = document.getElementById('total-deposite');
-    const addDeposite = document.getElementById('deposite-input');
+    const totalId = document.getElementById(depositeId);
+    const oldValue = parseFloat(totalId.innerText);
+    const newValue = oldValue + newDepositeValue;
+    totalId.innerText = newValue;
 
-    totalDeposite.innerText = parseFloat(totalDeposite.innerText) + parseFloat(addDeposite.value);
-    addDeposite.value = " ";
-    //total balance
+}
 
-    const totalBalance = document.getElementById('total-balance');
-    totalBalance.innerText = parseFloat(totalBalance.innerText) + parseFloat(totalDeposite.innerText);
+
+
+// add deposite function 
+document.getElementById('add-deposite').addEventListener('click', function () {
+    const depositeValue = getInput('deposite-input');
+    console.log(depositeValue);
+    updateField('total-deposite', depositeValue);
+
+});
+
+
+
+// add withdraw function 
+
+document.getElementById('add-withdraw').addEventListener('click', function () {
+    const withdrawValue = getInput('withdraw-input');
+    console.log(withdrawValue);
+    updateField('total-withdraw', withdrawValue);
 });
 
 
 
 
 
-// add witdraw
-
-document.getElementById('add-withdraw').addEventListener('click', function () {  //deposite button id = add-deposite
-
-    const totalWithdraw = document.getElementById('total-withdraw');
-    const addWithdraw = document.getElementById('withdraw-input');
 
 
 
-    totalWithdraw.innerText = parseFloat(totalWithdraw.innerText) + parseFloat(addWithdraw.value);
-
-    //Reduce From total balance 
-    const totalBalance = document.getElementById('total-balance');
-    totalBalance.innerText = parseFloat(totalBalance.innerText) - parseFloat(totalWithdraw.innerText);
-
-    addWithdraw.value = " ";
 
 
-});
+
+
+
+
+
+
+
+// // add deposite value
+
+// document.getElementById('add-deposite').addEventListener('click', function () {  //deposite button id = add-deposite
+
+//     const totalDeposite = document.getElementById('total-deposite');
+//     const addDeposite = document.getElementById('deposite-input');
+
+//     totalDeposite.innerText = parseFloat(totalDeposite.innerText) + parseFloat(addDeposite.value);
+//     addDeposite.value = " ";
+//     //total balance
+
+//     const totalBalance = document.getElementById('total-balance');
+//     totalBalance.innerText = parseFloat(totalBalance.innerText) + parseFloat(totalDeposite.innerText);
+// });
+
+
+
+
+
+// // add witdraw
+
+// document.getElementById('add-withdraw').addEventListener('click', function () {  //deposite button id = add-deposite
+
+//     const totalWithdraw = document.getElementById('total-withdraw');
+//     const addWithdraw = document.getElementById('withdraw-input');
+
+
+
+//     totalWithdraw.innerText = parseFloat(totalWithdraw.innerText) + parseFloat(addWithdraw.value);
+
+//     //Reduce From total balance
+//     const totalBalance = document.getElementById('total-balance');
+//     totalBalance.innerText = parseFloat(totalBalance.innerText) - parseFloat(totalWithdraw.innerText);
+
+//     addWithdraw.value = " ";
+
+
+// });
